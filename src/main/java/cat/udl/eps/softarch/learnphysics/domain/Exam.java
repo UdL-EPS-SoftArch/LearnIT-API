ae839e527fb1de8b5a8fae19df538d46a72139eb
Juan Boné <juanbone16@gmail.com>
2020-03-25 17:50:34 +0100

    Exam domain

--- src/main/java/cat/udl/eps/softarch/learnphysics/domain/Exam.java
+++ src/main/java/cat/udl/eps/softarch/learnphysics/domain/Exam.java
@@ -1,16 +1,29 @@
 package cat.udl.eps.softarch.learnphysics.domain;
 
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotBlank;
-import java.util.List;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-public class Exam {
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.mapping.List;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Table(name = "Exam")
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Exam extends UriEntity<Integer> {
     @Id
     private Integer examId;
 
     @NotBlank
     private Integer nbOfQuestions;
+
+    @Override
+    public Integer getId() {
+        return examId;
+    }
 
     //@NotBlank
     //@OneToMany
