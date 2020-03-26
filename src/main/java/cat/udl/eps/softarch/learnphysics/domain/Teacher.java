ed276bc0c1e8fdff7eef8566ac59bc8c844d6bd9
jboronat <j.boronat3@gmail.com>
2020-03-26 12:49:22 +0100

    Creating and editing Teacher

--- src/main/java/cat/udl/eps/softarch/learnphysics/domain/Teacher.java
+++ src/main/java/cat/udl/eps/softarch/learnphysics/domain/Teacher.java
@@ -1,7 +1,28 @@
 package cat.udl.eps.softarch.learnphysics.domain;
 
-import javax.persistence.Entity;
+
+import java.util.Collection;
+import javax.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 
 @Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class Teacher extends User {
+
+    @Column(unique=true)
+    private long id;
+
+    private String name_2;
+    private String creation_date;
+
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_TEACHER");
+    }
+
+
 }
