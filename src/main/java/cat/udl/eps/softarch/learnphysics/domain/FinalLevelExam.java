9f4af01d12ec5dbfb6974f5f58c330e2c222457f
jsolanes14 <jordisolanes14@gmail.com>
2020-03-27 16:41:37 +0100

    FinalLevelExam domain added

--- src/main/java/cat/udl/eps/softarch/learnphysics/domain/FinalLevelExam.java
+++ src/main/java/cat/udl/eps/softarch/learnphysics/domain/FinalLevelExam.java
@@ -1,18 +1,37 @@
 package cat.udl.eps.softarch.learnphysics.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Table(name = "FinalLevelExam")
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class FinalLevelExam extends Exam{
 
-    @NotBlank
-    //@OneToMany
-    private Integer level;
+    @Id
+    @Column(name="FinalLevelExam_Id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer FinalLevelExamId;
 
     @NotBlank
     private Float Mark;
 
     @NotBlank
     private String subject;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "FinalLevelExam")
+    private List<Level> level;
+
+    @Override
+    public Integer getId() {
+        return FinalLevelExamId;
+    }
 
 }
